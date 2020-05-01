d9408e60ffa775d2fdae69b64b4d08b6c3b94b4d
nick wuerz <nawuerz@gmail.com>
2020-05-01 17:51:44 -0500

    added manifest.json, added events to sw.js

---
+++ public/app.js
@@ -0,0 +1,17 @@
+//trilogy version
+
+// if ("serviceWorker" in navigator) {
+//     window.addEventListener("load", () => {
+//       navigator.serviceWorker.register("/sw.js").then(reg => {
+//         console.log("service worker registered", reg);
+//       });
+//     });
+//   }
+
+  //youtube version
+
+//   if('serviceWorker' in navigator) {
+//       navigator.serviceWorker.register('/sw.js')
+//       .then((reg) => console.log('service worker registered', reg))
+//       .catch((err) => console.log('service worker not registered', err));
+//   }
