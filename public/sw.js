e9827e14154673519d91a1c76fb2b77d74266131
nick wuerz <nawuerz@gmail.com>
2020-05-01 19:19:12 -0500

    added cache for shell assets

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,22 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+];
+
 //install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed');
+    // console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
 //activate event
@@ -10,5 +26,5 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    // console.log('fetch event', evt);
 });
